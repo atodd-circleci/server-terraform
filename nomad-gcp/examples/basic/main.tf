b48dee91ef75da6582b5854e448cf3f60c8dff0f
Nathan Smith <nathan@nfsmith.ca>
2021-03-17 18:00:10 -0700

    Add example of GCP nomad

---
+++ nomad-gcp/examples/basic/main.tf
@@ -0,0 +1,44 @@
+variable "project" {
+  type    = string
+  default = "nsmith-dev"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-c"
+}
+
+variable "network" {
+  type    = string
+  default = "default"
+}
+
+variable "server_endpoint" {
+  type    = string
+  default = "nomad.ns.sphereci.com:4647"
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+module "nomad" {
+  source = "./../../"
+
+  zone            = var.zone
+  region          = var.region
+  network         = var.network
+  server_endpoint = var.server_endpoint
+
+  unsafe_disable_mtls    = true
+  assign_public_ip       = true
+  preemptible            = true
+  target_cpu_utilization = 0.10
+}
